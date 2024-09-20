6a41a79d86a49cfd6ad5e775ec60bf03600da990
andreiculeva <83748812+andreiculeva@users.noreply.github.com>
2024-09-20 20:13:01 +0200

    try blackjack

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,14 +11,269 @@ import utils
 from humanfriendly import format_timespan
 import bot
 from discord import app_commands
+from enum import Enum
+
 
 allowed_guilds = (749670809110315019, 831556458398089217)
 
 
-
-
 red = discord.Color.red()
 orange = discord.Color.orange()
+
+
+emojis = {
+    "C2": 1286014015578968196,
+    "F2": 1286014048864829460,
+    "P2": 1286014076610412554,
+    "R2": 1286014086764564540,
+    "C3": 1286014098202562613,
+    "P3": 1286014110043078718,
+    "R3": 1286014126975483954,
+    "F3": 1286014151084343306,
+    "C4": 1286014177336496331,
+    "P4": 1286014188472500355,
+    "F4": 1286014206960992266,
+    "R4": 1286014219782979798,
+    "C5": 1286014228955795507,
+    "F5": 1286014237876949023,
+    "P5": 1286014250397077584,
+    "R5": 1286014261486817310,
+    "C6": 1286014274073792613,
+    "F6": 1286014285616648324,
+    "P6": 1286014298975637618,
+    "R6": 1286014309499011143,
+    "C7": 1286014324170690610,
+    "F7": 1286014336715722787,
+    "P7": 1286014349130993694,
+    "R7": 1286014357569929270,
+    "C8": 1286014366788878387,
+    "F8": 1286014378591911986,
+    "P8": 1286014390465724448,
+    "R8": 1286014401991671880,
+    "C9": 1286014411697295431,
+    "F9": 1286014420451070003,
+    "P9": 1286014434095136909,
+    "R9": 1286014445784666166,
+    "F10": 1286014455871967232,
+    "F10": 1286014464969146448,
+    "P10": 1286014475987845232,
+    "R10": 1286014484439367732,
+    "CA": 1286014500440375346,
+    "FA": 1286014509210927165,
+    "PA": 1286014578190323806,
+    "RA": 1286014591478005803,
+    "CJ": 1286014602664087677,
+    "FJ": 1286014610947706941,
+    "PJ": 1286014619625853039,
+    "RJ": 1286014663682691172,
+    "CK": 1286014679730229389,
+    "KF": 1286014689725124639,
+    "PK": 1286014699724607488,
+    "KR": 1286014709413183523,
+    "CQ": 1286014733618516039,
+    "FQ": 1286014742858825791,
+    "PQ": 1286014765759725588,
+    "RQ": 1286014778958938153,
+}
+
+
+class BlackJackStatus(Enum):
+    "Represents the win multiplier"
+    LOSE = -1
+    TIE = 0
+    WIN = 1
+    BLACKJACK = 1.5
+
+
+class Dealer:
+    def __init__(self, hand: list[str]):
+        self.hand = hand
+
+    @property
+    def score(self) -> int:
+        """
+        Logic copied from internet tbh but it seems to work
+
+        Min and max values in case there is an Ace on the hand.
+        Min will be calculated with Ace value as 1 and Max with Ace value as 11.
+        At the end the closest value to 21 without exceeding it, will be returned.
+        Otherwise, if there was no Ace, min and max are equals and any can be returned.
+        """
+        min = 0
+        max = 0
+        ace_flag = False
+        for card in self.hand:
+            if card[1] == "A" and ace_flag is True:
+                min += 1
+                max += 1
+            elif card[1] == "A" and ace_flag is False:
+                ace_flag = True
+                min += 1
+                max += 11
+            elif card[1] == "J" or card[1] == "Q" or card[1] == "K":
+                min += 10
+                max += 10
+            elif int(card[1:]) in range(2, 11):
+                min += int(card[1:])
+                max += int(card[1:])
+        if max == min:
+            score = min
+        elif max <= 21:
+            score = max
+        elif max > 21:
+            score = min
+        return score
+
+
+class Player(Dealer):
+    def __init__(self, hand: list[str], user_id: int, bet: int):
+        self.user_id: int = user_id
+        self.bet: int = bet
+        super().__init__(hand)
+
+
+class BlackJack(discord.ui.View):
+    def __init__(self, user: discord.User, bet: int, balance: int):
+        self.embed: discord.Embed = discord.Embed(
+            color=discord.Color.orange(), title=f"Blackjack (bet: {bet})"
+        )
+        self.end_game: bool = (
+            False  # only need this to know when to show dealer full hand
+        )
+        self.status: BlackJackStatus = BlackJackStatus.TIE
+        self.embed.set_author(name=user.display_name, icon_url=user.display_avatar)
+        self.embed.set_footer(text=f"Current balance: {balance - bet}")
+        self.initialize_game(user.id, bet)
+        super().__init__(timeout=None)
+
+    def update_embed(self) -> None:
+        self.embed.clear_fields()
+        hand = ""
+        self.user: discord.User
+        if self.end_game:
+            for card in self.dealer.hand:
+                card_emoji = emojis.get(card)
+                if card_emoji is None:
+                    hand = f"{hand} {card}"
+                else:
+                    hand = f"{hand} <:{card}:{card_emoji}>"
+            hand = f"{hand} ({self.dealer.score})"
+        else:
+            hand = self.dealer.hand[0]
+        self.embed.add_field(name="Dealer", value=hand, inline=False)
+        hand = ""
+        for card in self.player.hand:
+            card_emoji = emojis.get(card)
+            if card_emoji is None:
+                hand = f"{hand} {card}"
+            else:
+                hand = f"{hand} <:{card}:{card_emoji}>"
+        hand = f"{hand} ({self.player.score})"
+        self.embed.add_field(name="Player", value=hand, inline=False)
+
+    def initialize_game(self, user_id: int, bet: int) -> None:
+        """Shuffles 6 decks with 52 cards.
+        Gives the player cards"""
+        suits = ["F", "C", "R", "P"]
+        cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        self.deck: list = []
+        for suit in suits:
+            for card in cards:
+                self.deck.append(f"{suit}{card}")
+
+        self.deck *= 6
+        random.shuffle(self.deck)
+        self.dealer: Dealer = Dealer([self.deck.pop(), self.deck.pop()])
+        self.player: Player = Player([self.deck.pop(), self.deck.pop()], user_id, bet)
+        self.update_embed()
+
+        if (self.player.score != 21) and (self.dealer.score != 21):
+            return  # game goes on
+
+        if self.player.score == self.dealer.score == 21:  # TIE BLACKJACK
+            self.status = BlackJackStatus.TIE
+            self.embed.description = "Tie"
+        elif (self.dealer.score == 21) and (
+            self.player.score != 21
+        ):  # dealer blackjack
+            self.status = BlackJackStatus.LOSE
+            self.embed.description = "Dealer blackjack"
+        elif (self.dealer.score != 21) and (
+            self.player.score == 21
+        ):  # player blackjack
+            self.status = BlackJackStatus.BLACKJACK
+            self.embed.description = "Player blackjack"
+
+        self.end_game = True
+        self.hit.disabled = True
+        self.stand.disabled = True
+        self.stop()
+
+    @discord.ui.button(label="hit", style=discord.ButtonStyle.red)
+    async def hit(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.player.hand.append(self.deck.pop())
+        self.update_embed()
+
+        if self.player.score > 21:
+            self.end_game = True
+            self.embed.description = "Player busted"
+            self.status = BlackJackStatus.LOSE
+            self.hit.disabled = True
+            self.stand.disabled = True
+            await interaction.response.edit_message(embed=self.embed, view=self)
+            return self.stop()
+
+        elif self.player.score == 21:
+            return await self._stand(interaction)
+
+        await interaction.response.edit_message(view=self, embed=self.embed)
+
+    @discord.ui.button(label="stand", style=discord.ButtonStyle.primary)
+    async def stand(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await self._stand(interaction)
+
+    async def _stand(self, interaction: discord.Interaction):
+        """Function to avoid duplicated code"""
+        self.end_game = True
+        await self.deal_dealers_hand(interaction)
+        self.update_embed()
+        if self.dealer.score > self.player.score and self.dealer.score <= 21:
+            self.embed.description = "Dealer wins"
+            self.status = BlackJackStatus.LOSE
+        elif self.dealer.score == self.player.score:
+            self.status = BlackJackStatus.TIE
+            self.embed.description = "Tie"
+        else:
+            self.status = BlackJackStatus.WIN
+            self.embed.description = "Player wins"
+        self.stand.disabled = True
+        self.hit.disabled = True
+        if interaction.response.is_done():
+            await interaction.edit_original_response(embed=self.embed, view=self)
+        else:
+            await interaction.response.edit_message(embed=self.embed, view=self)
+        self.stop()
+
+    async def deal_dealers_hand(self, interaction: discord.Interaction):
+        """Function that will simulate the dealers play once the player stands"""
+
+        self.update_embed()
+        await interaction.response.edit_message(view=self, embed=self.embed)
+        while self.dealer.score < 17:
+
+            self.dealer.hand.append(self.deck.pop())
+            self.update_embed()
+            await interaction.edit_original_response(view=self, embed=self.embed)
+
+    async def interaction_check(
+        self, interaction: discord.Interaction[commands.Bot]
+    ) -> bool:
+        if interaction.user.id != self.player.user_id:
+            await interaction.response.send_message(
+                "This game is not for you", ephemeral=True
+            )
+            return False
+        return True
 
 
 class UrbanDictionaryPageSource(menus.ListPageSource):
@@ -98,8 +353,6 @@ class Fun(commands.Cog):
 
         pages = utils.RoboPages(UrbanDictionaryPageSource(data), ctx=ctx)
         await pages.start()
-
-
 
     @commands.command()
     async def c4(self, ctx: commands.Context, target: utils.MemberConverter):
@@ -520,9 +773,17 @@ class Fun(commands.Cog):
         else:
             await ctx.message.delete()
 
-
-
-
+    @commands.command(aliases=["bj"])
+    async def blackjack(self, ctx: commands.Context):
+        """Play blackjack (no betting for now)"""
+        game_view = BlackJack(ctx.author, 10, 100)
+        message = await ctx.send(view=game_view, embed=game_view.embed)
+        await game_view.wait()
+        # at  this point check outcome from view.status
+        game_view.embed.description += (
+            f"\nGame outcome: {game_view.status*game_view.player.bet}"
+        )
+        await message.edit(view=None, embed=game_view.embed)
 
     @commands.command()
     async def slidepuzzle(self, ctx: commands.Context, number: int = 3):
