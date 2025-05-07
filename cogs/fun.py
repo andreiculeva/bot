8c8259b0b3be9f2a5f9ae0050d793ebb80b4a972
andreiculeva <83748812+andreiculeva@users.noreply.github.com>
2025-05-07 20:42:59 +0200

    make user of type User

--- cogs/fun.py
+++ cogs/fun.py
@@ -928,7 +928,7 @@ class Fun(commands.Cog):
     @commands.hybrid_command(name="adminbirthday")
     @app_commands.guilds(discord.Object(id=831556458398089217))
     @commands.is_owner()
-    async def _admin(self, ctx:commands.Context, user:utils.UserConverter, birthdate:typing.Annotated[datetime.date, utils.DateConverter]):
+    async def _admin(self, ctx:commands.Context, user:discord.User, birthdate:typing.Annotated[datetime.date, utils.DateConverter]):
         """owner command to set birthdays"""
         await self.bot.pool.execute(
             """
