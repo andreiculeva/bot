0a877101bcf3fa20c89341001a0c9e69aa27e8a3
andreiculeva <83748812+andreiculeva@users.noreply.github.com>
2025-06-21 09:06:54 +0200

    test

--- cogs/events.py
+++ cogs/events.py
@@ -76,6 +76,7 @@ class events(commands.Cog):
         birthday_channel_ids = await self.bot.pool.fetch("SELECT channel_id FROM birthday_channels")
         print(f"Channels = {birthday_channel_ids}")
         for channel_id in birthday_channel_ids:
+            print(f"Channel id = {channel_id}")
             channel = self.bot.get_channel(channel_id)
             if channel is None:
                 continue
